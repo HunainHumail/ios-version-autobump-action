76628047ec9954f3c6673a341a0ee6e4d1d07387
HunainHumail <hunainhumail786@gmail.com>
2022-08-27 02:45:33 +0500

    fixed currentVersionName

--- index.js
+++ index.js
@@ -102,7 +102,7 @@ Toolkit.run(async (tools) => {
     console.log('file content: ',versionCodeRegexPattern.exec(fileContent)[0]);
 
     // let currentVersionName = semver.clean(versionCodeRegexPattern.exec(fileContent.toString())[1]);
-    let currentVersionName = fileContent.toString().replace('CURRENT_PROJECT_VERSION = ','');
+    let currentVersionName = versionCodeRegexPattern.exec(fileContent)[0].toString().replace('CURRENT_PROJECT_VERSION = ','');
     console.log(`Current version: ${currentVersionName}`);
 
     let newVersionName = semver.inc(currentVersionName, "minor");
